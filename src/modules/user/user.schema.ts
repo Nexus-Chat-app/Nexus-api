f003547e199b9399bba85fe712ce8e7070142989
Zaiidmo <Zaiidmoumnii@gmail.com>
2024-11-12 12:25:46 +0100

    Create User Schema

---
+++ src/modules/user/user.schema.ts
@@ -0,0 +1,24 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document, Types } from 'mongoose';
+
+@Schema()
+export class User extends Document {
+  @Prop({ required: true, unique: true })
+  username: string;
+
+  @Prop({ required: true, unique: true })
+  email: string;
+
+  @Prop({ required: true, unique: true })
+  phone: string;
+
+  @Prop({ type: [Types.ObjectId], ref: 'User', default: [] })
+  friends: Types.ObjectId[]; // Array of user ids
+
+  @Prop({ required: true })
+  password: string;
+
+  @Prop({ default: false })
+  isOnline : boolean;
+
+} 
