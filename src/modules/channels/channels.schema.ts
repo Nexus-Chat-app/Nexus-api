cabb68819236e052c10ad053c00367aaf383d4c6
Zaiidmo <Zaiidmoumnii@gmail.com>
2024-11-12 15:20:00 +0100

    Create the Channels Schema

--- src/modules/channels/channels.schema.ts
+++ src/modules/channels/channels.schema.ts
@@ -1,3 +1,21 @@
-/* 
-    MongoDB Schema for Channels
-*/
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document, Types } from 'mongoose';
+
+@Schema()
+export class Channel extends Document {
+  @Prop({ required: true })
+    name: string;
+
+    @Prop({ type: Boolean, required: true })
+    isPublic: boolean;
+
+    @Prop({type: Types.ObjectId, ref: 'User', required: true})
+    owner: Types.ObjectId;
+
+    @Prop({type: Types.ObjectId, ref: 'User', default: []})
+    admins: Types.ObjectId;
+
+    @Prop({type: [Types.ObjectId], ref: 'User', default: []})
+    members: Types.ObjectId[];
+
+}
