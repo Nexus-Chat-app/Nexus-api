946fa5d00d24df26c9f5979be06415d2f1946d1c
Zaiidmo <Zaiidmoumnii@gmail.com>
2024-11-12 20:08:13 +0100

    DATABASE: Create the db configuration

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -1,3 +1,19 @@
 /* 
     Database configuration file
 */
+
+import { MongooseModule } from "@nestjs/mongoose";
+import { ConfigModule, ConfigService } from "@nestjs/config";
+
+export const DatabaseConfig = MongooseModule.forRootAsync({
+
+    imports:[ConfigModule],
+    useFactory: (configService: ConfigService) => ({
+        uri: configService.get<string>('DB_URI_ATLAS'),
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    }),
+
+    inject: [ConfigService],
+
+});
