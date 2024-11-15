e3da6b00352e338dd1b91ff37097065351f623fa
Zaiidmo <Zaiidmoumnii@gmail.com>
2024-11-15 15:54:42 +0100

    Create Util Function for types conversion

--- src/shared/utils.ts
+++ src/shared/utils.ts
@@ -1,3 +1,13 @@
 /* 
     Utility functions (data formatting, ID generation, etc...)
 */
+
+import { Types } from 'mongoose';
+
+export function toObjectId(id: string | Types.ObjectId): Types.ObjectId {
+  return typeof id === 'string' ? new Types.ObjectId(id) : id;
+}
+
+export function toStringId(id: string | Types.ObjectId): string {
+    return id.toString();
+}
