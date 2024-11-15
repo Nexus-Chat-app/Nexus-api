9699277a27dd908a418ca1601e3f4531dc8c0662
Zaiidmo <Zaiidmoumnii@gmail.com>
2024-11-15 12:45:35 +0100

    RT-CHAT: Creating the chat gateway file for sockets configuration

--- src/modules/chat/chat.gateway.ts
+++ src/modules/chat/chat.gateway.ts
@@ -1,3 +1,39 @@
 /* 
     WebSocket gateway for real-time chat 
 */
+import { 
+    WebSocketGateway,
+    SubscribeMessage,
+    MessageBody,
+    WebSocketServer,
+    OnGatewayConnection,
+    OnGatewayDisconnect
+} from '@nestjs/websockets';
+import { Server, Socket} from 'socket.io';
+import { ChatService } from './chat.service';
+
+@WebSocketGateway({
+    namespace: '/chat',
+    cors: { 
+        origin: '*'
+    },
+})
+
+export class ChatGateway implements OnGatewayConnection, OnGatewayDisconnect {
+    @WebSocketServer()
+    server: Server;
+
+    constructor(private readonly chatService: ChatService) {}
+
+    handleConnection(client: Socket) {
+        console.log(`Client connected: ${client.id}`);
+    }
+    handleDisconnect(client: Socket) {
+        console.log(`Client disconnected: ${client.id}`);
+    }
+
+    @SubscribeMessage('sendMessage')
+  async handleSendMessage(
+    @MessageBody() messageData: { senderId: string; channelId: string; content: string },
+  ) { }
+}
