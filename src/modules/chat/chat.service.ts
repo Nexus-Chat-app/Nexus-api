4c3e4ef62fbe5c34f468f20bee5b9c60c1b3483d
Zaiidmo <Zaiidmoumnii@gmail.com>
2024-11-15 15:35:40 +0100

    Creating Chat Service

--- src/modules/chat/chat.service.ts
+++ src/modules/chat/chat.service.ts
@@ -3,15 +3,40 @@
 */
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
-import { Model } from 'mongoose';
-import { Chat } from './chat.schema';
+import { Model, Types } from 'mongoose';
+import { Chat, ChatDocument } from './chat.schema';
 
 @Injectable()
 export class ChatService {
-  constructor(@InjectModel(Chat.name) private chatModel: Model<Chat>) {}
+  constructor( 
+    @InjectModel(Chat.name) private chatModel: Model<ChatDocument>,
+  ) {}
 
-  async createMessage(messageData: Partial<Chat>): Promise<Chat> {
-    const message = new this.chatModel(messageData);
-    return message.save();
+  async createChat(data: {
+    content: string,
+    sender: Types.ObjectId,
+    receiver?: Types.ObjectId,
+    channel?: string,
+  }) { 
+    const chat = new this.chatModel(data);
+    return chat.save();
+  }
+
+  async getChatsByChannel(channelId: string){
+    return this.chatModel
+    .find({ channelId: new Types.ObjectId(channelId) })
+    .populate('sender', 'username')
+    .sort({createdAt: -1})
+  }
+
+  async getChatsBetweenUsers(userId1: string, userId2: string){
+    return this.chatModel.find({
+      $or: [
+        { sender: new Types.ObjectId(userId1), receiver: new Types.ObjectId(userId2) },
+        { sender: new Types.ObjectId(userId2), receiver: new Types.ObjectId(userId1) }
+      ]
+    })
+    .populate('sender', 'username')
+    .sort({createdAt: -1})
   }
 }
