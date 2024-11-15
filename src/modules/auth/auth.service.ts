0c5cef5670a16b88f5f85d4d5f5ae9e26d2a9806
Zaiidmo <Zaiidmoumnii@gmail.com>
2024-11-15 17:09:10 +0100

    Creating the Auth Service;

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,3 +1,34 @@
 /* 
     Business logic for authentication
 */
+
+import { Injectable } from '@nestjs/common';
+import { HttpService } from '@nestjs/axios';
+import { AxiosResponse } from 'axios';
+
+@Injectable()
+export class AuthService { 
+    constructor(private readonly httpService: HttpService) {}
+
+    async registerUser(userData: any): Promise<AxiosResponse> {
+        const authServiceUrl = `process.env.AUTH_SERVICE_URL/register`;
+
+        try { 
+            const response = await this.httpService.post(authServiceUrl, userData).toPromise();
+            return response;
+        } catch ( error ) {
+            throw new Error('Error registering user: ${error.message}');
+        }
+    }
+
+    async loginUser(userData: any): Promise<AxiosResponse> {
+        const authServiceUrl = `process.env.AUTH_SERVICE_URL/login`;
+
+        try { 
+            const response = await this.httpService.post(authServiceUrl, userData).toPromise();
+            return response;
+        } catch ( error ) {
+            throw new Error('Error logging in user: ${error.message}');
+        }
+    }
+}
