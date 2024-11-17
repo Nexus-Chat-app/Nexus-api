4e113c02c3a0af103d97c1e0a4bc94eabdf41792
Zaiidmo <Zaiidmoumnii@gmail.com>
2024-11-17 12:39:32 +0100

    Updating dependencies

--- src/events/rabbitmq.events.ts
+++ src/events/rabbitmq.events.ts
@@ -1,39 +1,50 @@
 import { Injectable, OnModuleInit } from '@nestjs/common';
-import * as amqp from 'amqplib';
-import { log } from 'console';
+import { AmqpConnection } from '@golevelup/nestjs-rabbitmq';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { User } from '../modules/user/user.schema';
 
 @Injectable()
 export class RabbitMQEvents implements OnModuleInit {
-  private channel: amqp.Channel;
+  constructor(
+    private readonly amqpConnection: AmqpConnection,
+    @InjectModel(User.name) private readonly userModel: Model<User>,
+  ) {}
 
   async onModuleInit() {
-    const connection = await amqp.connect(process.env.RABBITMQ_URL);
-    this.channel = await connection.createChannel();
-    await this.channel.assertExchange('auth-events', 'fanout', {
+    // Assert exchange and queue
+    const channel = await this.amqpConnection.createChannel();
+    await channel.assertExchange('auth-events', 'fanout', { durable: false });
+    const queue = await channel.assertQueue('chat-service-queue', {
       durable: false,
     });
+    await channel.bindQueue(queue.queue, 'auth-events', '');
 
-    const queue = await this.channel.assertQueue('', { exclusive: true });
-    this.channel.bindQueue(queue.queue, 'auth-events', '');
+    // Consume messages
+    channel.consume(queue.queue, async (msg) => {
+      if (msg) {
+        const event = JSON.parse(msg.content.toString());
+        console.log('Received event:', event);
 
-    this.channel.consume(
-      queue.queue,
-      (msg) => {
-        if (msg) {
-          const event = JSON.parse(msg.content.toString());
-          this.handleEvent(event);
+        if (event.eventType === 'USER_REGISTERED') {
+          await this.handleUserRegistered(event.payload);
         }
-      },
-      { noAck: true },
-    );
-
-    console.log('Connected to RabbitMQ and listening for events...');
+        channel.ack(msg); // Acknowledge message
+      }
+    });
   }
 
-  private async handleEvent(event: any) {
-    console.log('Received event:', event);
-    if(event.eventType === 'USER_REGISTERED') {
-        console.log('User registered:', event.data);
+  async handleUserRegistered(payload: any) {
+    const { id, username, email } = payload;
+
+    // Check if the user already exists in the chat service database
+    const existingUser = await this.userModel.findOne({ _id: id });
+    if (!existingUser) {
+      // Add the new user to the Chat Service database
+      await this.userModel.create({ _id: id, username, email });
+      console.log('User synced to Chat Service database:', payload);
+    } else {
+      console.log('User already exists in Chat Service database:', id);
     }
   }
 }
