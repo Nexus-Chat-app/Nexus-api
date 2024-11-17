62bca53f2e23d9c6be0db3a91ffda4667257a929
Zaiidmo <Zaiidmoumnii@gmail.com>
2024-11-17 13:12:28 +0100

    Create the auth Controller

--- src/api/auth.controller.ts
+++ src/api/auth.controller.ts
@@ -1,3 +1,37 @@
 /* 
     Authentication endpoints
 */
+
+import { Controller, Post, Body } from "@nestjs/common";
+import { AuthService } from "../modules/auth/auth.service";
+
+@Controller('auth')
+export class AuthController {
+    constructor( private readonly authService: AuthService) {}
+
+    //Register a new user
+    @Post('register')
+    async registerUser(@Body() userData: {
+        username: string;
+        email: string;
+        phoneNumber: string;
+        password: string;
+    }) {
+        try{ const result = await this.authService.registerUser(userData);
+            return { message: ' User registered successfully', user: result };
+        } catch (error) {
+            return { message: 'Error registering user', error: error.message };
+        }
+    }
+
+    //Login a user
+    @Post('login')
+    async loginUser(@Body() credentials: { identifier: string; password: string }) {
+        try {
+            const result = await this.authService.loginUser(credentials);
+            return { message: 'User logged in successfully', user: result };
+        } catch (error) {
+            return { message: 'Error logging in user', error: error.message };
+        }
+    }
+}
