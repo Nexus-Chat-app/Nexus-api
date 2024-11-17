ecd3b4d70cf4f25d46867bc0105df2c8b1874c53
Zaiidmo <Zaiidmoumnii@gmail.com>
2024-11-17 13:08:50 +0100

    Updating Auth.module and auth.service for axios calls

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -1,3 +1,14 @@
 /* 
     MongoDB model for user authentication 
 */
+
+import { Module } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { HttpModule } from '@nestjs/axios';
+
+@Module({
+    imports: [HttpModule],
+    providers: [AuthService],
+    exports: [AuthService]
+})
+export class AuthModule {}
--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -5,30 +5,53 @@
 import { Injectable } from '@nestjs/common';
 import { HttpService } from '@nestjs/axios';
 import { AxiosResponse } from 'axios';
+import { firstValueFrom } from 'rxjs';
 
 @Injectable()
-export class AuthService { 
-    constructor(private readonly httpService: HttpService) {}
+export class AuthService {
+  private authServiceUrl = 'http://localhost:3000';
+  constructor(private readonly httpService: HttpService) {}
 
-    async registerUser(userData: any): Promise<AxiosResponse> {
-        const authServiceUrl = `process.env.AUTH_SERVICE_URL/register`;
-
-        try { 
-            const response = await this.httpService.post(authServiceUrl, userData).toPromise();
-            return response;
-        } catch ( error ) {
-            throw new Error('Error registering user: ${error.message}');
-        }
+  async registerUser(userData: {
+    username: string;
+    email: string;
+    phoneNumber: string;
+    password: string;
+  }): Promise<AxiosResponse> {
+    try {
+      const response = await firstValueFrom(
+        this.httpService.post(`${this.authServiceUrl}/register`, userData),
+      );
+      return response;
+    } catch (error) {
+      throw new Error('Error registering user: ${error.message}');
     }
+  }
 
-    async loginUser(userData: any): Promise<AxiosResponse> {
-        const authServiceUrl = `process.env.AUTH_SERVICE_URL/login`;
+  async loginUser(credentials: { identifier: string; password: string }): Promise<AxiosResponse> {
+    const authServiceUrl = `process.env.AUTH_SERVICE_URL/login`;
 
-        try { 
-            const response = await this.httpService.post(authServiceUrl, userData).toPromise();
-            return response;
-        } catch ( error ) {
-            throw new Error('Error logging in user: ${error.message}');
-        }
+    try {
+      const response = await firstValueFrom(
+        this.httpService.post(`${this.authServiceUrl}/login`, credentials),
+      );
+      return response;
+    } catch (error) {
+      throw new Error('Error logging in user: ${error.message}');
     }
+  }
+
+  // Verify the JWT token
+  async authenticate(token: string): Promise<any> {
+    const tokenValidationUrl = `process.env.AUTH_SERVICE_URL/verify/check-token`;
+
+    try {
+      const response = await this.httpService
+        .post(tokenValidationUrl, { token })
+        .toPromise();
+      return response.data;
+    } catch (error) {
+      throw new Error('Error authenticating user: ${error.message}');
+    }
+  }
 }
