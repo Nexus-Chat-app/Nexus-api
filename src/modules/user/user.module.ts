7b04a2ef23bf35350a53349f1e836350a0160967
fatima zahra cherkaoui <cherkaoui.fatimazahra97@gmail.com>
2024-11-20 16:38:13 +0100

    add userService and also exported in user module

--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -2,10 +2,13 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UserService } from './user.service';
 import { UserSchema } from './user.schema';
+import { UserController } from '../../api/user.controller';
+import { UserGateway } from '../../gateway/user/user.gateway';
 
 @Module({
   imports: [MongooseModule.forFeature([{ name: 'User', schema: UserSchema }])],
-  providers: [UserService],
-  exports: [UserService], 
+  controllers: [UserController],
+  providers: [UserService, UserGateway],
+  exports: [UserService, MongooseModule],
 })
 export class UserModule {}
