2d54349bc7f54ed6dbe8162de5fc8b0b21dc0d36
fatima zahra cherkaoui <cherkaoui.fatimazahra97@gmail.com>
2024-11-20 16:44:02 +0100

    import userModel and provide channelService also add the controllers for channelController

--- src/modules/channel/channel.module.ts
+++ src/modules/channel/channel.module.ts
@@ -6,10 +6,17 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ChannelService } from './channel.service';
 import { ChannelSchema } from './channel.schema';
+import { UserModule } from '../user/user.module';
+import { ChannelController } from '../../api/channels.controller';
+import { ChannelGateway } from '../../gateway/channel/channel.gateway';
 
 @Module({
-  imports: [MongooseModule.forFeature([{ name: 'Channel', schema: ChannelSchema }])],
-  providers: [ChannelService],
-  exports: [ChannelService], 
+  imports: [
+    MongooseModule.forFeature([{ name: 'Channel', schema: ChannelSchema }]),
+    UserModule,
+  ],
+  providers: [ChannelService, ChannelGateway],
+  exports: [ChannelService],
+  controllers: [ChannelController],
 })
 export class ChannelModule {}
